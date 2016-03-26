89da0a7fa50a5bf2c271347d410d893789966b87
KGjertsson <gjertsson.konrad@gmail.com>
2016-03-26 23:02:49 +0100

    testing

--- Card.py
+++ Card.py
@@ -6,6 +6,12 @@ Created on Sat Mar 26 22:11:25 2016
 """
 
 class Card:
+    """
+    Heart = 0
+    Spades = 1
+    Clubs = 2    
+    Diamonds 3
+    """
     suite = -1
     value = -1
     
---
+++ Test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 26 22:36:40 2016
+"""
+
+import Card
+card1 = Card(1, 1)
+card2 = Card(2, 2)
+card1.display()
+card2.displayCard()
