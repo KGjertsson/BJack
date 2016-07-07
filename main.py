4e5bf0c7e6fe3de54423b28c11bb2b376e252357
Konrad Gjertsson <Konrad Gjertsson>
2016-07-08 00:00:43 +0200

    mmmmmm

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 26 22:36:40 2016
-"""
-
 #from classes.card import Card
 #from classes.deck import Deck
 from classes.gameEngine import GameEngine
 import tkinter as tk
+import tkinter as tk
 
-engine = GameEngine()
-
+if __name__ == "__main__":
+    engine = GameEngine()
