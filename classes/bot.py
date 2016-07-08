5c8fe023e0c99968e78a3c52bb9bede8751b4d49
KGjertsson <gjertsson.konrad@gmail.com>
2016-07-08 21:59:12 +0200

    added bot class

---
+++ classes/bot.py
@@ -0,0 +1,4 @@
+class jack:
+
+    def __init__(self):
+        print('My name is Jack, Black Jack, thanks for creating me')
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #from classes.card import Card
 #from classes.deck import Deck
 from classes.gameEngine import GameEngine
+from classes.bot import jack
 import tkinter as tk
 import tkinter as tk
+
 
 if __name__ == "__main__":
     engine = GameEngine()
     engine.readConfig()
     engine.createPlayers()
+    bot = jack()
